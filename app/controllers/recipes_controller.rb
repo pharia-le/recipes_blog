d40e972cca7e446c5830838ff196f3df1f26277b
pharia-le <pharia.le@gmail.com>
2020-04-22 17:33:30 -0700

    Modify config.ru to include Rack::MethodOverride Middleware. Add routes & modify views.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,4 +5,23 @@ class RecipesController < ApplicationController
         erb :'/recipes/index'
     end
 
+    get '/recipes/new' do
+        erb :'/recipes/new'
+    end
+
+    get '/recipes/:id' do
+        @recipe = Recipe.find_by_id(params[:id])
+        erb :'/recipes/show'
+    end
+
+    post '/recipes' do
+        recipe = Recipe.create(params[:recipe])
+        redirect "/recipes/#{recipe.id}"
+    end
+
+    delete '/recipes/:id' do
+        Recipe.find_by_id(params[:id]).destroy
+        redirect '/recipes'
+    end
+    
 end
